caa267af2b458d535941228137b33f47a0a952e4
madlhawa <madhawa242@gmail.com>
2021-05-19 09:26:18 -0700

    Added utility

---
+++ Utility/Create_Core.sh
@@ -0,0 +1,14 @@
+#Create core
+cd /opt/bitnami/solr/server/solr/
+sudo -u solr mkdir reviewCore
+sudo -u solr cp -r ./configsets/_default/conf* ./reviewCore/
+
+#Install Git
+sudo apt update
+sudo -u solr apt install git
+sudo apt install git
+git clone https://github.com/Madlhawa/Hotel-Recommendation-System.git
+
+#Post Documents
+cd /opt/bitnami/solr/
+sudo -u solr ./bin/post -c reviewCore /home/aprabhath029/Hotel-Recommendation-System/Data/booking/booking.csv 
